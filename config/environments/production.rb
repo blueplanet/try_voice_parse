4aef27c2d3df7f6639205ae46a560ac265cde001
blueplanet <erguolinge@gmail.com>
2017-03-09 06:37:47 +0900

    rename

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "rails5base_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "try_voice_parse_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
