28541dc279c0a7a82a724419ca34dc06d57f7dbd
blueplanet <erguolinge@gmail.com>
2017-03-13 06:16:31 +0900

    Fix to local wav file

--- app/models/google_speech.rb
+++ app/models/google_speech.rb
@@ -1,11 +1,11 @@
 class GoogleSpeech
-  def post
+  def post(wav_path)
     res = Faraday.post do |req|
       req.url "https://speech.googleapis.com/v1beta1/speech:syncrecognize?key=#{ENV['GOOGLE_API_KEY']}"
       req.headers['Content-Type'] = 'application/json'
       req.body = {
-        config: { encoding: 'FLAC', sample_rate: 16000 },
-        audio: { uri: 'gs://cloud-samples-tests/speech/brooklyn.flac' }
+        config: { encoding: 'LINEAR16', sample_rate: 16000 },
+        audio: { content: Base64.strict_encode64(File.read(wav_path)) }
       }.to_json
     end
 
