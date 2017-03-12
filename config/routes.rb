57208c822ec4f1343e20af8b81f51da07a89796b
blueplanet <erguolinge@gmail.com>
2017-03-13 07:20:56 +0900

    Add voices

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :voices
 
-  root to: 'static_pages#top'
+  devise_for :users
+
+  root to: 'voices#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822215503) do
+ActiveRecord::Schema.define(version: 20170312221907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 20160822215503) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "voices", force: :cascade do |t|
+    t.string   "name"
+    t.string   "wav"
+    t.string   "lang"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
